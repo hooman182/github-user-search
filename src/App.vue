a760c568bd9db297830e37bfbdeb77d488187252
hooman <hooman182@gmail.com>
2021-08-29 00:58:31 +0430

    feat: Create index view and import to appt

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
+  <div class="min-h-screen bg-gray-100">
+    <Index />
+  </div>
 </template>
 
 <script>
-
+import Index from "@/views/Index";
 export default {
   name: "App",
+  components: {Index}
 };
 </script>
---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen flex flex-col items-center justify-center">
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
