70d32ee82383f6fdb561049c6d148e2de37ff1d4
hooman <hooman182@gmail.com>
2021-08-29 19:44:07 +0430

    feat: Create TheHeader component
    Create TheHeader component
    
    Import TheHeader component to index view

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,72 @@
+<template>
+  <header class="w-full flex justify-between items-center">
+    <h1 class="text-xl font-semibold text-gray-700 dark:text-white">
+      Github users finder
+    </h1>
+    <button
+      class="flex items-center font-medium text-gray-500 dark:text-white"
+      @click="themeMode"
+    >
+      {{ darkMode }}
+      <template v-if="darkMode == 'Dark'">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 ml-2"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+          />
+        </svg>
+      </template>
+      <template v-else>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 ml-2"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </template>
+    </button>
+  </header>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+import { onBeforeMount } from "@vue/runtime-core";
+export default {
+  name: "TheHeader",
+  setup() {
+    const darkMode = ref("Dark");
+    function themeMode() {
+      if (localStorage.theme == "" || localStorage.theme == undefined) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+        darkMode.value = "Light";
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.removeItem("theme");
+        darkMode.value = "Dark";
+      }
+      console.log(localStorage.theme);
+    }
+    onBeforeMount(() => {
+      if (localStorage.theme == "dark") {
+        document.documentElement.classList.add("dark");
+        darkMode.value = "Light";
+      }
+    });
+    return { themeMode, darkMode };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="h-screen flex flex-col items-center justify-center">
-      
+  <div class="h-screen w-1/2 m-auto flex flex-col items-center justify-center">
+      <TheHeader />
   </div>
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader";
 export default {
-
+  name: "Index",
+  components: {TheHeader}
 }
 </script>
 
