32264630767a68d70b3b88ff82130ccc3809e5cb
hooman <hooman182@gmail.com>
2021-08-29 20:28:38 +0430

    feat: Create new component
    Create SearchBar component
    
    Import SearchBar component to index view

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <form method="post" class="w-full my-4">
+    <div class="bg-white dark:bg-opacity-20 flex items-center rounded-lg p-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 ml-2 text-blue-500"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+      <input type="text" placeholder="Search github username..." class="bg-white bg-opacity-0 flex-1 mx-4 outline-none font-semibold dark:text-gray-400"/>
+      <button type="submit" class="btn-blue">Search</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,27 @@
 <template>
-  <div class="h-screen w-1/2 m-auto flex flex-col items-center justify-center">
-      <TheHeader />
+  <div
+    class="
+      h-screen
+      w-full
+      lg:w-1/2
+      p-5
+      lg:p-0
+      lg:m-auto
+      flex flex-col
+      items-center
+      justify-center
+    "
+  >
+    <TheHeader />
+    <SearchBar />
   </div>
 </template>
 
 <script>
 import TheHeader from "@/components/TheHeader";
+import SearchBar from "@/components/SearchBar";
 export default {
   name: "Index",
-  components: {TheHeader}
-}
+  components: { TheHeader, SearchBar },
+};
 </script>
-
-<style>
-
-</style>
