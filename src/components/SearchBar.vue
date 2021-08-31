94173b69bc621711a61ea020ec8c47868f3da257
hooman <hooman182@gmail.com>
2021-08-31 23:25:34 +0430

    Form Handle and get input value in SearchBar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <form method="post" class="w-full my-4">
+  <form method="post" class="w-full my-4" @submit.prevent="searchFormHandle">
     <div class="bg-white dark:bg-opacity-10 flex items-center rounded-lg p-2">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -15,12 +15,35 @@
           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
         />
       </svg>
-      <input type="text" placeholder="Search github username..." class="bg-white bg-opacity-0 flex-1 mx-4 outline-none font-semibold dark:text-gray-400"/>
+      <input
+        type="text"
+        placeholder="Search github username..."
+        class="
+          bg-white bg-opacity-0
+          flex-1
+          mx-4
+          outline-none
+          font-semibold
+          dark:text-gray-400
+        "
+        v-model="searchValue"
+      />
       <button type="submit" class="btn-blue">Search</button>
     </div>
   </form>
 </template>
 
 <script>
-export default {};
+import { ref } from '@vue/reactivity';
+export default {
+  name:"SearchBar",
+  setup(props, {emit}) {
+    const searchValue = ref('');
+    function searchFormHandle() {
+      emit('searchHandle', searchValue.value);
+      searchValue.value = "";
+    }
+    return{searchValue, searchFormHandle}
+  }
+};
 </script>
